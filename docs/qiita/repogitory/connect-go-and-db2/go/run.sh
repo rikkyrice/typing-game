65fdb0b59cc9428de96b9e60fb3da831d9f54926
RIKU <rikkyrice@gmail.com>
2020-10-29 13:14:01 +0900

    fix: Qiita編集

--- docs/qiita/repogitory/connect-go-and-db2/go/run.sh
+++ docs/qiita/repogitory/connect-go-and-db2/go/run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-export PATH=../clidriver/bin:$PATH
+
+case $(uname | tr '[:upper:]' '[:lower:]') in
+    darwin*) export OS='macos';;
+    mingw*) export OS='win'
+esac
+
+if [ OS = 'macos' ]; then
+    export DB2HOME=../clidriver
+    export CGO_CFLAGS=-I${DB2HOME}/include
+    export CGO_LDFLAGS=-L${DB2HOME}/lib
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${DB2HOME}/lib
+else
+    export PATH=../clidriver/bin:$PATH
+fi
 
 go mod vendor
 
