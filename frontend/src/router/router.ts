d29d90c6a4784b04b2ba6612ff73c928c7115ef4
Riku Hashiki <riku.hashiki@ibm.com>
2021-02-06 19:35:22 +0900

    topページと登録ページとログインページを追加

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -4,8 +4,22 @@ export default [
   {
     path: '/',
     name: PAGES.TOP,
-    component: () => import('@/components/pages/HelloWorld.vue'),
+    component: () => import('@/components/pages/TopView.vue'),
     meta: { title: 'TOP', saveScroll: false },
+    props: true,
+  },
+  {
+    path: '/signup',
+    name: PAGES.SIGNUP,
+    component: () => import('@/components/pages/SignupView.vue'),
+    meta: { title: 'SIGNUP', saveScroll: false },
+    props: true,
+  },
+  {
+    path: '/login',
+    name: PAGES.LOGIN,
+    component: () => import('@/components/pages/LoginView.vue'),
+    meta: { title: 'LOGIN', saveScroll: false },
     props: true,
   },
   {
