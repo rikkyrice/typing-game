b013a55b4ea9434d789d2ccecf2927dd1049179e
Riku Hashiki <riku.hashiki@ibm.com>
2021-03-02 00:42:58 +0900

    wordArrayを初期化

--- frontend/src/components/pages/TypingGameView.vue
+++ frontend/src/components/pages/TypingGameView.vue
@@ -14,7 +14,7 @@
     <div class="pa-0" style="width: 100%;">
       <typing-game-content
         :wordList="wordList"
-        :wordArray="wordArray"
+        :wordArray="getWordArray"
       />
     </div>
     <lwtg-loader :page-loading="true" :loading="false" />
@@ -24,6 +24,8 @@
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import { mixins } from 'vue-class-component';
+import WordListApi from '@/api/wordlist';
+import WordApi from '@/api/word';
 import LwtgBreadcrumbs from '@/components/atoms/LwtgBreadcrumbs.vue';
 import LwtgLoader from '@/components/atoms/LwtgLoader.vue';
 import LwtgWordCreateButton from '@/components/atoms/LwtgWordCreateButton.vue';
@@ -31,7 +33,7 @@ import TypingGameContent from '@/components/organisms/TypingGameContent.vue';
 import UtilMixin from '@/mixins/utilMixin';
 import { BreadcrumbInfo } from '@/models/types/breadcrumbInfo';
 import { WordList } from '@/models/wordlist';
-import { WordArray } from '@/models/word';
+import { WordArray, initializedWordArray } from '@/models/word';
 
 @Component({
   components: {
@@ -42,34 +44,51 @@ import { WordArray } from '@/models/word';
   },
 })
 export default class WordListView extends mixins(UtilMixin) {
-  wordList: WordList = {
-      id: '1',
-      title: 'TOEIC',
-      explanation: 'TOEICの頻出単語を単語帳にしました！ぜひ使って990点めざしましょう!',
-      createdAt: '2021-02-06-00:00:00',
-      updatedAt: '2021-02-06-00:00:00',
-  }
+  wordList: WordList = {} as WordList;
   wordArray: WordArray = {
-    matched: 1,
-    words: [
-      {
-        id: '8e97230e-2c9d-44ed-9a95-e39aa6217dd7',
-        word: '橋木陸',
-        yomi: 'はしきりく',
-        meaning: '2020年度IBMの社員',
-        mYomi: '2020ねんどIBMのしゃいん',
-        explanation: '2020年に日本IBMに入社した社員の一人。',
-        isRemembered: true,
-        createdAt: '2020-10-03-21.50.00.000000',
-        updatedAt: '2020-10-03-21.50.00.000000',
-      },
-    ]
+    matched: 0,
+    words: [],
+  }
+  wordListLoading = false;
+  wordsLoading = false;
+  get getWordArray() {
+    return this.wordArray.matched !== 0
+      ? this.wordArray
+      : initializedWordArray
+  }
+  get viewLoading() {
+    return (
+      this.wordListLoading ||
+      this.wordsLoading
+    );
+  }
+  created() {
+    this.fetchGetWordList();
+    this.fetchGetWords();
+  }
+  fetchGetWordList() {
+    this.wordListLoading = true;
+    WordListApi.getWordList(this.$route.params.wordlistId)
+      .then((data) => {
+        this.wordList = data;
+        this.breadcrumbs.push(
+          { label: `${this.wordList.title}`, path: `/mypage/${this.wordList.id}` },
+          { label: `タイピングゲーム`, path: `/mypage/${this.wordList.id}/typing-game}` },
+        );
+      })
+      .finally(() => (this.wordListLoading = false));
+  }
+  fetchGetWords() {
+    this.wordsLoading = true;
+    WordApi.getWords(this.$route.params.wordlistId)
+      .then((data) => {
+        this.wordArray = data;
+      })
+      .finally(() => (this.wordsLoading = false));
   }
   breadcrumbs: BreadcrumbInfo[] = [
     { label: 'トップ', path: '/' },
     { label: 'My Page', path: '/mypage' },
-    { label: `${this.wordList.title}`, path: `/mypage/${this.wordList.id}` },
-    { label: `タイピングゲーム`, path: `/mypage/${this.wordList.id}/typing-game}` },
   ];
 }
 </script>
--- frontend/src/components/pages/WordListView.vue
+++ frontend/src/components/pages/WordListView.vue
@@ -14,17 +14,19 @@
     <div class="pa-0" style="width: 100%;">
       <word-content
         :wordList="wordList"
-        :wordArray="wordArray"
+        :wordArray="getWordArray"
       />
     </div>
     <lwtg-word-create-button />
-    <lwtg-loader :page-loading="true" :loading="false" />
+    <lwtg-loader :page-loading="true" :loading="viewLoading" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import { mixins } from 'vue-class-component';
+import WordListApi from '@/api/wordlist';
+import WordApi from '@/api/word';
 import LwtgBreadcrumbs from '@/components/atoms/LwtgBreadcrumbs.vue';
 import LwtgLoader from '@/components/atoms/LwtgLoader.vue';
 import LwtgWordCreateButton from '@/components/atoms/LwtgWordCreateButton.vue';
@@ -32,7 +34,7 @@ import WordContent from '@/components/organisms/WordContent.vue';
 import UtilMixin from '@/mixins/utilMixin';
 import { BreadcrumbInfo } from '@/models/types/breadcrumbInfo';
 import { WordList } from '@/models/wordlist';
-import { WordArray } from '@/models/word';
+import { WordArray, initializedWordArray } from '@/models/word';
 
 @Component({
   components: {
@@ -43,66 +45,51 @@ import { WordArray } from '@/models/word';
   },
 })
 export default class WordListView extends mixins(UtilMixin) {
-  wordList: WordList = {
-      id: '1',
-      title: 'TOEIC',
-      explanation: 'TOEICの頻出単語を単語帳にしました！ぜひ使って990点めざしましょう!',
-      createdAt: '2021-02-06-00:00:00',
-      updatedAt: '2021-02-06-00:00:00',
-  }
+  wordList: WordList = {} as WordList;
   wordArray: WordArray = {
-    matched: 4,
-    words: [
-      {
-        id: '8e97230e-2c9d-44ed-9a95-e39aa6217dd7',
-        word: 'organization',
-        yomi: '',
-        meaning: '組織、企業',
-        mYomi: 'かきくけこ',
-        explanation: 'おーがにぜーしょん',
-        isRemembered: true,
-        createdAt: '2020-10-03-21.50.00.000000',
-        updatedAt: '2020-10-03-21.50.00.000000',
-      },
-      {
-        id: '90443017-7096-4e67-9ada-863f581bd1a7',
-        word: 'communication',
-        yomi: '',
-        meaning: 'コミュニケーション',
-        mYomi: 'かきくけこ',
-        explanation: '大事',
-        isRemembered: false,
-        createdAt: '2020-10-03-21.51.00.000000',
-        updatedAt: '2020-10-03-21.51.00.000000',
-      },
-      {
-        id: 'b5f888bd-d8c6-4d74-a61c-ef838d8b52d9',
-        word: 'government',
-        yomi: '',
-        meaning: '陰謀ありあり',
-        mYomi: 'かきくけこ',
-        explanation: '政府',
-        isRemembered: false,
-        createdAt: '2020-10-03-21.52.00.000000',
-        updatedAt: '2020-10-03-21.52.00.000000',
-      },
-      {
-        id: '48d4becc-f73c-45f7-8cb5-a9dee4c4f214',
-        word: 'authentication',
-        yomi: '',
-        meaning: 'ここには何でも',
-        mYomi: 'かきくけこ',
-        explanation: '認証',
-        isRemembered: true,
-        createdAt: '2020-10-03-21.53.00.000000',
-        updatedAt: '2020-10-03-21.53.00.000000',
-      }
-    ]
+    matched: 0,
+    words: [],
+  }
+  wordListLoading = false;
+  wordsLoading = false;
+  get getWordArray() {
+    console.log(this.wordArray);
+    return this.wordArray.matched !== 0
+      ? this.wordArray
+      : initializedWordArray
+  }
+  get viewLoading() {
+    return (
+      this.wordListLoading ||
+      this.wordsLoading
+    );
+  }
+  created() {
+    this.fetchGetWordList();
+    this.fetchGetWords();
+  }
+  fetchGetWordList() {
+    this.wordListLoading = true;
+    WordListApi.getWordList(this.$route.params.wordlistId)
+      .then((data) => {
+        this.wordList = data;
+        this.breadcrumbs.push(
+          { label: `${this.wordList.title}`, path: `/mypage/${this.wordList.id}` },
+        );
+      })
+      .finally(() => (this.wordListLoading = false));
+  }
+  fetchGetWords() {
+    this.wordsLoading = true;
+    WordApi.getWords(this.$route.params.wordlistId)
+      .then((data) => {
+        this.wordArray = data;
+      })
+      .finally(() => (this.wordsLoading = false));
   }
   breadcrumbs: BreadcrumbInfo[] = [
     { label: 'トップ', path: '/' },
     { label: 'My Page', path: '/mypage' },
-    { label: `${this.wordList.title}`, path: `/mypage/${this.wordList.id}` },
   ];
 }
 </script>
